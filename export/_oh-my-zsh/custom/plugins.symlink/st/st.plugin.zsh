7e3f89c49c1dd3ba77be43dd8cd63d7762a20531
Maksim Ryzhikov <rv.maksim@gmail.com>
2013-05-04 04:18:20 +0400

    maksimrv   M export/_zsh.symlink/1zshinit ?? export/_oh-my-zsh/custom/plugins.symlink/st/

---
+++ export/_oh-my-zsh/custom/plugins.symlink/st/st.plugin.zsh
@@ -0,0 +1,52 @@
+# Super touch
+if [ ! -d "$TEMPLATE_DIR" ]; then
+    export TEMPLATE_DIR="$HOME/.template"
+fi
+
+# param {String} fileName
+# param {String} template
+function _st_write(){
+  local fileName="$1"
+  local template="$2"
+
+  echo "$template" >> "$fileName"
+}
+
+# Get template by file name
+# in folder ~/.templates
+#
+# param {String} fileName
+# return {String} The template string
+function _st_getTemplate(){
+    local templateDir="$TEMPLATE_DIR"
+    local fileName="$1"
+    local TEMPLATE=""
+
+    if [ -e "$templateDir" ]; then
+      for tmpFileName in $(ls -A "$templateDir"); do
+        if [ -e "$templateDir/$tmpFileName" ]; then
+          if [ "$tmpFileName" == "$fileName" ];then
+            TEMPLATE="$(cat $templateDir/$tmpFileName)"
+          fi
+        fi
+      done
+    fi
+
+    echo "$TEMPLATE"
+}
+
+
+# My awesome touch function
+function st(){
+  local template=""
+
+  for fileName in $@; do
+      template="$(_st_getTemplate $fileName)"
+
+      if [ "$template" ]; then
+        _st_write $fileName "$template"
+      fi
+  done
+
+  touch $@
+}
