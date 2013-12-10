f3a42744b1b0e5dc54066ea6864ab68f81d38a37
Maksim Ryzhikov <rv.maksim@gmail.com>
2013-12-11 01:54:40 +0400

    Revert "maksimrv   M lib/dot_commit.bash"
    This reverts commit f0a57f60be88f51b1e0842eb6b812000db58e594.

--- lib/dot_commit.bash
+++ lib/dot_commit.bash
@@ -32,7 +32,7 @@ function dot_commit() {
 
     git add -A
     git commit -m "${message}"
-    git push origin
+    git push origin --progress
     echo "Wooh $USER...Changes was saved successfully!"
   else
     echo 'You did not change anything'
