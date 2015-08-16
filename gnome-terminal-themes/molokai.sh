832cdd9bdf85354c876f55f4eec40af47cef4485
Maksim Ryzhikov <rv.maksim@gmail.com>
2015-08-17 02:32:51 +0300

    maksimrv  M  gnome-terminal-themes/molokai.sh

--- gnome-terminal-themes/molokai.sh
+++ gnome-terminal-themes/molokai.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Molokai theme for gnome-terminal
-gnome_term_profile=/apps/gnome-terminal/profiles/Default
+profile_name=${1:-'Default'}
+gnome_term_profile=/apps/gnome-terminal/profiles/$profile_name
 
 palette="#1B1B1D1D1E1E:#F9F926267272:#8282B4B41414:#FDFD97971F1F:#5656C2C2D6D6:#8C8C5454FEFE:#BABABDBDB6B6:#CCCCCCCCC6C6:#505053535454:#FFFF59599595:#B6B6E3E35454:#FEFEEDED6C6C:#8C8CEDEDFFFF:#9E9E6F6FFEFE:#89899C9CA1A1:#F8F8F8F8F2F2"
 bd_color="#000000000000"
