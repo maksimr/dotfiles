8a361e4e1722330af09b0bb39004ac585270db01
Maksim Ryzhikov <rv.maksim@gmail.com>
2024-03-27 01:09:00 +0300

    M .config/powershell/Microsoft.PowerShell_profile.ps1

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,10 @@
-Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -EditMode Vi
+
+Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardChar
+Set-PSReadLineKeyHandler -Chord "Ctrl+e" -Function EndOfLine
+Set-PSReadLineKeyHandler -Chord "Ctrl+a" -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Chord "Ctrl+w" -Function BackwardDeleteWord
+
+Set-PSReadLineKeyHandler -ViMode Command -Chord "Ctrl+e" -Function EndOfLine
+Set-PSReadLineKeyHandler -ViMode Command -Chord "Ctrl+a" -Function BeginningOfLine
+Set-PSReadLineKeyHandler -ViMode Command -Chord "Ctrl+w" -Function BackwardDeleteWord
