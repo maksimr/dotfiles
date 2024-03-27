3648b4541d037de0d05d5dafda5a2ff8302c5939
Maksim Ryzhikov <rv.maksim@gmail.com>
2024-03-27 15:27:15 +0300

    Update Microsoft.PowerShell_profile.ps1

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,14 @@
+$env:EDITOR = $env:VISUAL = 'vim'
+
 Set-PSReadLineOption -EditMode Vi
 
+Set-PSReadLineKeyHandler -Chord "Ctrl+r" -Function ReverseSearchHistory
 Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardChar
 Set-PSReadLineKeyHandler -Chord "Ctrl+e" -Function EndOfLine
 Set-PSReadLineKeyHandler -Chord "Ctrl+a" -Function BeginningOfLine
 Set-PSReadLineKeyHandler -Chord "Ctrl+w" -Function BackwardDeleteWord
 
+Set-PSReadLineKeyHandler -ViMode Command -Chord "Ctrl+r" -Function ReverseSearchHistory
 Set-PSReadLineKeyHandler -ViMode Command -Chord "Ctrl+e" -Function EndOfLine
 Set-PSReadLineKeyHandler -ViMode Command -Chord "Ctrl+a" -Function BeginningOfLine
 Set-PSReadLineKeyHandler -ViMode Command -Chord "Ctrl+w" -Function BackwardDeleteWord
