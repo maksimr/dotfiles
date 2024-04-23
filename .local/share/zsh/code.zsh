317af684d3affd8114873e4b127b360fd1f995aa
Maksim Ryzhikov <rv.maksim@gmail.com>
2024-04-23 10:35:41 +0300

    Create code.zsh

---
+++ .local/share/zsh/code.zsh
@@ -0,0 +1,17 @@
+if [ "$(command -v wslpath)"  ]; then
+  function code() {
+    VSCODE= [ "$(command -v code)" ] && echo 'code' || echo 'cmd.exe /c code'
+    WSL_DISTRO_NAME=$(wslpath -m / | awk -F/ '{print $4}')
+
+    VSCODE_ARGS=()
+    for arg in "$@"; do
+      if [ -f "$arg" ] || [ -d "$arg" ]; then
+        VSCODE_ARGS+=("$(wslpath -a "$FILEPATH")")
+      else
+        VSCODE_ARGS+=("$arg")
+      fi
+    done
+
+    $VSCODE --remote wsl+$WSL_DISTRO_NAME "${VSCODE_ARGS[@]}"
+  }
+fi
