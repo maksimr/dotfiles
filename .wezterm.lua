685214016b6fa7f529625d03ebc174039cc3391b
Maksim Ryzhikov <rv.maksim@gmail.com>
2024-09-29 15:13:27 +0300

    M .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,7 +12,7 @@ config.command_palette_font_size = 28.0
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
 
-local opacity = 0.50
+local opacity = 0.65
 config.window_background_opacity = opacity
 config.text_background_opacity = opacity
 local blur = 7
@@ -41,6 +41,83 @@ config.window_padding = {
   bottom = 0,
 }
 config.window_close_confirmation = "NeverPrompt"
+
+config.colors = {
+  -- The default text color
+  foreground = '#c7c7c7',
+  -- The default background color
+  background = '#333333',
+
+  -- Overrides the cell background color when the current cell is occupied by the
+  -- cursor and the cursor style is set to Block
+  cursor_bg = '#52ad70',
+  -- Overrides the text color when the current cell is occupied by the cursor
+  cursor_fg = '#feffff',
+  -- Specifies the border color of the cursor when the cursor style is set to Block,
+  -- or the color of the vertical or horizontal bar when the cursor style is set to
+  -- Bar or Underline.
+  cursor_border = '#52ad70',
+
+  -- the foreground color of selected text
+  selection_fg = 'black',
+  -- the background color of selected text
+  selection_bg = '#c6dcfc',
+
+  -- The color of the scrollbar "thumb"; the portion that represents the current viewport
+  scrollbar_thumb = '#222222',
+
+  -- The color of the split lines between panes
+  split = '#444444',
+
+  ansi = {
+    '#000000', -- Black
+    '#b83019', -- Red
+    '#51bf37', -- Green
+    '#c6c43d', -- Yellow
+    '#0c24bf', -- Blue
+    '#b93ec1', -- Magenta
+    '#53c2c5', -- Cyan
+    '#c7c7c7', -- White
+  },
+  brights = {
+    '#676767', -- Black
+    '#ef766d', -- Red
+    '#8cbc87', -- Green
+    '#fefb7e', -- Yellow
+    '#6a71f6', -- Blue
+    '#f07ef8', -- Magenta
+    '#95d2d4', -- Cyan
+    '#feffff', -- White
+  },
+
+  -- Arbitrary colors of the palette in the range from 16 to 255
+  indexed = { [136] = '#af8700' },
+
+  -- Since: 20220319-142410-0fcdea07
+  -- When the IME, a dead key or a leader key are being processed and are effectively
+  -- holding input pending the result of input composition, change the cursor
+  -- to this color to give a visual cue about the compose state.
+  compose_cursor = 'orange',
+
+  -- Colors for copy_mode and quick_select
+  -- available since: 20220807-113146-c2fee766
+  -- In copy_mode, the color of the active text is:
+  -- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
+  -- 2. selection_* otherwise
+  copy_mode_active_highlight_bg = { Color = '#000000' },
+  -- use `AnsiColor` to specify one of the ansi color palette values
+  -- (index 0-15) using one of the names "Black", "Maroon", "Green",
+  --  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
+  -- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
+  copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
+  copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
+  copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
+
+  quick_select_label_bg = { Color = 'peru' },
+  quick_select_label_fg = { Color = '#ffffff' },
+  quick_select_match_bg = { AnsiColor = 'Navy' },
+  quick_select_match_fg = { Color = '#ffffff' },
+}
 
 -- override opacity by cmd+u
 config.keys = {
