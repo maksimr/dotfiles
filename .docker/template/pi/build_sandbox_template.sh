cb77ce83fe5ccd361bd7050b1f5f3cfe1938fbfe
Maksim Ryzhikov <rv.maksim@gmail.com>
2026-07-20 14:41:10 +0300

    M .docker/template/pi/build_sandbox_template.sh

--- .docker/template/pi/build_sandbox_template.sh
+++ .docker/template/pi/build_sandbox_template.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 TEMPLATE_NAME="pi"
-TEMPLATE_VERSION="v1"
 CURRENT_FILE_PATH="$(realpath "${BASH_SOURCE[0]}")"
 DOCKER_TEMPLATE_DIR="$(dirname "$CURRENT_FILE_PATH")"
 TEMP_DIR="$(mktemp -d "${TMPDIR:-/tmp}/pi-docker-build.XXXXXX")"
 TEMPLATE_TAR_PATH="$TEMP_DIR/${TEMPLATE_NAME}.tar"
 
-docker build -t ${TEMPLATE_NAME}:${TEMPLATE_VERSION} "$DOCKER_TEMPLATE_DIR"
-docker image save ${TEMPLATE_NAME}:${TEMPLATE_VERSION} -o "$TEMPLATE_TAR_PATH"
+docker build -t ${TEMPLATE_NAME} "$DOCKER_TEMPLATE_DIR"
+docker image save ${TEMPLATE_NAME} -o "$TEMPLATE_TAR_PATH"
 sbx template load "$TEMPLATE_TAR_PATH"
