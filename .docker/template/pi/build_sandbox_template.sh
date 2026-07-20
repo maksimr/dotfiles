fda12da0a630ef776cd17b927ae59d4ffbca9070
Maksim Ryzhikov <rv.maksim@gmail.com>
2026-07-20 12:37:09 +0300

    D  .docker/kit/pi/spec.yaml A  .docker/template/pi/Dockerfile A  .docker/template/pi/build_sandbox_template.sh

---
+++ .docker/template/pi/build_sandbox_template.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+TEMPLATE_NAME="pi"
+TEMPLATE_VERSION="v1"
+CURRENT_FILE_PATH="$(realpath "${BASH_SOURCE[0]}")"
+DOCKER_TEMPLATE_DIR="$(dirname "$CURRENT_FILE_PATH")"
+TEMP_DIR="$(mktemp -d "${TMPDIR:-/tmp}/pi-docker-build.XXXXXX")"
+TEMPLATE_TAR_PATH="$TEMP_DIR/${TEMPLATE_NAME}.tar"
+
+docker build -t ${TEMPLATE_NAME}:${TEMPLATE_VERSION} "$DOCKER_TEMPLATE_DIR"
+docker image save ${TEMPLATE_NAME}:${TEMPLATE_VERSION} -o "$TEMPLATE_TAR_PATH"
+sbx template load "$TEMPLATE_TAR_PATH" --name "$TEMPLATE_NAME" --force
