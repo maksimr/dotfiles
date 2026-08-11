ecd321bb2229a5b4b0508276f7ce3070b4feb25f
Maksim Ryzhikov <rv.maksim@gmail.com>
2026-08-11 10:17:01 +0300

    feat(bootstrap): use local udot binary for dotfile sync

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,22 +3,4 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-if [ "$(command -v npx)" ]; then
-  npx -y github:maksimr/udot apply --base-dir="$(dirname "${BASH_SOURCE}")"
-  exit 0
-fi
-
-function doIt() {
-  rsync \
-    --exclude ".git/" \
-    --exclude ".gitignore" \
-    --exclude ".DS_Store" \
-    --exclude ".osx" \
-    --exclude ".ignore" \
-    --exclude "bootstrap.sh" \
-    --exclude "README.md" \
-    -avh --no-perms . "$HOME";
-}
-
-doIt;
-unset doIt;
+./.local/bin/udot apply --base-dir="$(dirname "${BASH_SOURCE}")"
