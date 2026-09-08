137f4aee1cc2d97b56ab8c1b53b48fa8c53d8535
Maksim Ryzhikov <rv.maksim@gmail.com>
2026-09-09 00:31:54 +0300

    feat(send-file-path-to-terminal): add script to send file path and selection range to terminal

---
+++ .config/joyride/scripts/send_file_path_to_terminal.js
@@ -0,0 +1,19 @@
+//@ts-check
+const vscode = require('vscode');
+
+async function main() {
+  const editor = vscode.window.activeTextEditor;
+  const terminal = vscode.window.activeTerminal;
+  if (!editor || !terminal) return;
+  const filePath = editor.document.uri.fsPath;
+  const selection = editor.selection;
+  const start = selection.start.line + 1;
+  const end = selection.end.line + 1;
+  const range = (selection.isEmpty) ? '' : (start === end ? `:${start}` : `:${start}-${end}`);
+  terminal.sendText(filePath + range + ' ', false);
+  terminal.show();
+}
+
+exports.main = main;
+
+main();
